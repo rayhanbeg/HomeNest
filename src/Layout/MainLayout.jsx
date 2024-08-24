8b52ab6717cac3080744a62a1fd9a1b7e7353f16
rayhanbeg <mdrayhan40301@gmail.com>
2024-08-24 23:56:53 +0600

    add: loader for mainLayout

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,8 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import { useEffect, useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 const MainLayout = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [])
+
+    if(loading) {
+        return (
+            <div className="flex justify-center items-center h-screen bg-gray-800 opacity-75"><PulseLoader color="#ffffff" /></div>
+        )
+    }
     return (
         <div>
             <Navbar/>
