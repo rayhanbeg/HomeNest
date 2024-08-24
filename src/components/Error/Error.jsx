c51e8741b2ec031618834a8a49456e62f572bf17
rayhanbeg <mdrayhan40301@gmail.com>
2024-08-24 18:25:37 +0600

    done: error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,18 +1,36 @@
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const Error = () => {
+	const navigate = useNavigate()
     return (
-        <section className="flex items-center h-full sm:p-16 dark:text-gray-800">
-	<div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-40 h-40 dark:text-gray-400">
-			<path fill="currentColor" d="M256,16C123.452,16,16,123.452,16,256S123.452,496,256,496,496,388.548,496,256,388.548,16,256,16ZM403.078,403.078a207.253,207.253,0,1,1,44.589-66.125A207.332,207.332,0,0,1,403.078,403.078Z"></path>
-			<rect width="176" height="32" x="168" y="320" fill="currentColor"></rect>
-			<polygon fill="currentColor" points="210.63 228.042 186.588 206.671 207.958 182.63 184.042 161.37 162.671 185.412 138.63 164.042 117.37 187.958 141.412 209.329 120.042 233.37 143.958 254.63 165.329 230.588 189.37 251.958 210.63 228.042"></polygon>
-			<polygon fill="currentColor" points="383.958 182.63 360.042 161.37 338.671 185.412 314.63 164.042 293.37 187.958 317.412 209.329 296.042 233.37 319.958 254.63 341.329 230.588 365.37 251.958 386.63 228.042 362.588 206.671 383.958 182.63"></polygon>
-		</svg>
-		<p className="text-3xl">Looks like our services are currently offline</p>
-		<a rel="noopener noreferrer" href="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Back to homepage</a>
-	</div>
-</section>
+		<section className="bg-white ">
+		<div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
+			<div className="flex flex-col items-center max-w-sm mx-auto text-center">
+				<p className="p-3 text-sm font-medium text-blue-500 rounded-full bg-blue-50">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+					</svg>
+				</p>
+				<h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">Page not found</h1>
+				<p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn't exist. Here are some helpful links:</p>
+	
+				<div className="flex items-center w-full mt-6 gap-x-3 shrink-0 sm:w-auto">
+					<button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border roundedLg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:rotate-180">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+						</svg>
+	
+	
+						<span onClick={() => navigate(-1)}>Go back</span>
+					</button>
+	
+					<Link to='/' className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 roundedLg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+						Take me home
+					</Link>
+				</div>
+			</div>
+		</div>
+	</section>
     );
 };
 
